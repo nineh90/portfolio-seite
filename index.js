d9d43c66ef8c02ea60c4fb8cbb2add56dba850a4
nineh90 <102158158+nineh90@users.noreply.github.com>
2022-09-19 16:26:46 +0200

    first Styling

---
+++ index.js
@@ -0,0 +1,12 @@
+function init(){
+    welcomeText.innerHTML = `<h1><span  id="txtWrapper">Willkommen,</span></h1>
+                            <h2>hier entsteht die Portfolio-Seite<br>
+                            von <span>Nils Nehring</span>
+                            </h2>`
+                            ; 
+    console.log('init')
+}
+
+function alert(){
+    alert('Befindet sich noch im Aufbau')
+}
