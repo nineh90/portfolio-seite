4b7f02073c7d989851c4d1e657d98b5f9c212b31
nineh90 <102158158+nineh90@users.noreply.github.com>
2022-09-22 13:38:23 +0200

    test

--- script/script.js
+++ script/script.js
@@ -1,9 +1,58 @@
 function init(){
-    welcomeText.innerHTML = `<div class="screen-wrapper"><h1><span  id="txtWrapper">Willkommen,</span></h1>
-        <h2>hier entsteht die Portfolio-Seite<br>
-        von <span>Nils Nehring</span>
-        </h2></div>`
-        ; 
+    welcomeText.innerHTML = `<h1 class="ml11">
+    <span class="text-wrapper">
+      <span class="line line1"></span>
+      <span class="letters">Wilkommen
+      </span>
+    </span>
+    </h1>
+    <h1 class="ml11">
+    <span class="text-wrapper">
+      <span class="line line1"></span>
+      <span class="letters">Wilkommen
+      </span>
+    </span>
+    </h1>`
+    // Wrap every letter in a span
+var textWrapper = document.querySelector('.ml11 .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml11 .line',
+    scaleY: [0,1],
+    opacity: [0.5,1],
+    easing: "easeOutExpo",
+    duration: 700
+  })
+  .add({
+    targets: '.ml11 .line',
+    translateX: [0, document.querySelector('.ml11 .letters').getBoundingClientRect().width + 10],
+    easing: "easeOutExpo",
+    duration: 700,
+    delay: 100
+  }).add({
+    targets: '.ml11 .letter',
+    opacity: [0,1],
+    easing: "easeOutExpo",
+    duration: 600,
+    offset: '-=775',
+    delay: (el, i) => 34 * (i+1)
+  }).add({
+    targets: '.ml11',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
+
+  
+    //    welcomeText.innerHTML = `<div class="screen-wrapper"><h1><span  id="txtWrapper">Willkommen,</span></h1>
+    //                             <h2>hier entsteht die Portfolio-Seite<br>
+    //                                 von <span>Nils Nehring</span>
+    //                             </h2>
+    //                         </div>`
+    //     ; 
 }
 
 function sendMail(){
